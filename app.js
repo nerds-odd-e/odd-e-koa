2631c89acb44ec3acdfe1162ebd39bddc9b72137
Jackson Zhang <zbcjackson@gmail.com>
2017-03-22 14:06:35 +0800

    Implement live rooms by socket

--- app.js
+++ app.js
@@ -1,1 +1,64 @@
-require('babel-register');import Koa from 'koa'import convert from "koa-convert";import cors from 'kcors'import bodyParser from 'koa-bodyparser'import onError from "koa-onerror";import json from "koa-json";import logger from "koa-logger";import router from "./routes";const app = new Koa();app.use(convert(cors()))app.use(bodyParser());app.use(json());app.use(logger());app.use(async (ctx, next) => {    const start = new Date();    await next();    const ms = new Date() - start;    console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);});app.use(router.allowedMethods())app.use(router.routes())onError(app)app.listen(3000, () => console.log("Start on http://localhost:3000"))export default app
+require('babel-register');
+import Koa from 'koa'
+import convert from "koa-convert"
+import cors from 'kcors'
+import bodyParser from 'koa-bodyparser'
+import onError from "koa-onerror"
+import json from "koa-json"
+import logger from "koa-logger"
+import IO from 'koa-socket'
+import uuid from 'node-uuid'
+
+import router from "./routes"
+
+const app = new Koa()
+const io = new IO()
+
+app.use(convert(cors()))
+app.use(bodyParser())
+app.use(json())
+app.use(logger())
+
+app.use(router.allowedMethods())
+app.use(router.routes())
+
+onError(app)
+io.attach(app)
+
+io.use(async (ctx, next) => {
+    console.log(`From ${ctx.socket.id} - ${ctx.event}`)
+    await next()
+})
+
+var rooms = {}
+
+io.on('connection', (ctx, data) => {
+    console.log(`${ctx.socket.id} connected`)
+})
+
+io.on('disconnect', (ctx, data) => {
+    let {socket: {socket}} = ctx
+    console.log(`${socket.id} disconnected`)
+    if (socket.roomKey) {
+        console.log(`Close room: ${rooms[socket.roomKey].title}`)
+        delete rooms[socket.roomKey]
+    }
+})
+
+io.on('create_room', (ctx, data) => {
+    let {socket: {socket}} = ctx
+    console.log(socket.join)
+    console.log(`Create room: ${data.title}`)
+    var roomKey = uuid.v4()
+    rooms[roomKey] = data
+    socket.roomKey = roomKey
+    socket.join(roomKey)
+})
+
+router.get('/rooms', (ctx, next) => {
+    ctx.body = rooms
+})
+
+app.listen(3000, () => console.log("Start on http://localhost:3000"))
+
+export default app
